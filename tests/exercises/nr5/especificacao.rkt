2859dc135065214b26d86317bfb6e68ad63b420a
lohamsilva-ufop <loham.silva@aluno.ufop.edu.br>
2024-06-27 20:11:09 -0300

    27-06-2024

--- tests/exercises/nr5/especificacao.rkt
+++ tests/exercises/nr5/especificacao.rkt
@@ -3,3 +3,4 @@
 quantidade-execucoes: 1;
 gabarito: "C:/Users/Loham Santos da Silv/Documents/GitHub/symb/tests/exercises/nr5/gabarito/5.py";
 dir-aluno-exercicios: "C:/Users/Loham Santos da Silv/Documents/GitHub/symb/tests/exercises/nr5/exercicio-alunos";
+
