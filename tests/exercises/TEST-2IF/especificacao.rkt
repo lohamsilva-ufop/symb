cff45422b1979d21a2c21731037d482a09413d69
Loham Santos da Silva <loham.silva@aluno.ufop.edu.br>
2024-10-11 20:06:22 -0300

    11-10-2024

---
+++ tests/exercises/TEST-2IF/especificacao.rkt
@@ -0,0 +1,5 @@
+#lang symb/especificacao/conf
+
+quantidade-execucoes: 1;
+gabarito: "C:/Users/Loham Santos da Silv/Documents/GitHub/symb/tests/exercises/TEST-2IF/gabarito/gab.py";
+dir-aluno-exercicios: "C:/Users/Loham Santos da Silv/Documents/GitHub/symb/tests/exercises//TEST-2IF/exercicio-alunos";
---
+++ tests/exercises/TEST-2IF/exercicio-alunos/correto.py
@@ -0,0 +1,12 @@
+A = input("Digite o valor A: ")
+B = input("Digite o valor B: ")
+
+if A > 0 :
+    print (A)
+else :
+    print ("Menor")
+        
+if B < 0 :
+    print (B)
+else :
+    print ("Maior")
